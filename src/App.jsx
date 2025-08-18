d6cbe687c24597f19155ac993467a9cbda88c11a
Felipe Gyyn <felipegyyn@gmail.com>
2025-08-18 14:37:33 -0300

    alter new new

--- src/App.jsx
+++ src/App.jsx
@@ -365,7 +365,7 @@ function App() {
       </div>
 
       {/* Chat Container */}
-      <div className="max-w-5xl mx-auto px-4 py-2">
+      <div className="max-w-5xl mx-auto px-10 py-1">
         <div className="bg-white rounded-2xl shadow-xl border border-gray-200 overflow-hidden">
           {/* Messages */}
           <div className="h-[600px] overflow-y-auto p-6 space-y-4">
